899077d47f60cc4fed3ade4b16dd181effcf5efb
Natnael Feleke <myscreateline@gmail.com>
2021-08-12 22:36:55 +0300

    adding mapbox to airbnb

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibmF0aS1ib3kzMyIsImEiOiJja3M5MnVvNDgxc3g1MnZwaHppcGphN212In0.l1zpo4EeS-2-J7f4ID4Lzg",
+  },
 };
